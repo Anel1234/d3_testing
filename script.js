e088deff449987253ec8e104555d49c94da6ba89
GIDEV\apilav <anel.pilav@gmail.com>
2019-04-01 16:09:15 +0100

    added new reorder function and gradual data addition

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ console.log("test");
 
 var margin, width, height;
 var data;
+var svg;
+var x, y;
 
 window.onload = () => {
 
@@ -15,16 +17,16 @@ window.onload = () => {
         height = 500 - margin.top - margin.bottom;
 
     // set the ranges
-    var x = d3.scaleBand()
+    x = d3.scaleBand()
         .range([0, width])
         .padding(0.1);
-    var y = d3.scaleLinear()
+    y = d3.scaleLinear()
         .range([height, 0]);
 
     // append the svg object to the body of the page
     // append a 'group' element to 'svg'
     // moves the 'group' element to the top left margin
-    var svg = d3.select(".chart").append("svg")
+    svg = d3.select(".chart").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -68,6 +70,25 @@ window.onload = () => {
                 .attr("fill", (d) => {
                     return getColor(1 - d.sales / 100);
                 });
+
+            svg.selectAll("text")
+                .data(data)
+                .enter()
+                .append("text")
+                .text(function (d) {
+                    return d.sales;
+                })
+                .attr("class", "chartLabel")
+                .attr("text-anchor", "middle")
+                .attr("x", function (d, i) {
+                    return x(d.salesperson) + x.bandwidth() / 2;
+                })
+                .attr("y", function (d) {
+                    return y(d.sales) - 10;
+                })
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "11px")
+                .attr("fill", "black");
 
             //add the x Axis
             svg.append("g")
@@ -138,11 +159,18 @@ window.onload = () => {
 
 }
 
-// updateChartRepeat = () => {
-//     setInterval(() => {
-//         updateChart();
-//     }, 2000);
-// }
+updateChartRepeat = () => {
+    setInterval(() => {
+        randomlyAddToData();
+        slideBars();
+    }, 2000);
+}
+
+randomlyAddToData = () => {
+    data.forEach(d => {
+        d.sales += randomIntFromInterval(0,5);
+    });
+}
 
 
 updateChart = () => {
@@ -150,11 +178,11 @@ updateChart = () => {
     data = randomiseData(data);
     data = reorderData(data);
 
-    var x = d3.scaleBand()
-        .range([0, width])
-        .padding(0.1);
-    var y = d3.scaleLinear()
-        .range([height, 0]);
+    // var x = d3.scaleBand()
+    //     .range([0, width])
+    //     .padding(0.1);
+    // var y = d3.scaleLinear()
+    //     .range([height, 0]);
 
     data.forEach(function (d) {
         d.sales = +d.sales;
@@ -183,7 +211,10 @@ updateChart = () => {
     bars.enter()
         .append("rect")
         .attr("class", "bar")
-        .attr("x", width)
+        // .attr("x", width)
+        .attr("x", (d) => {
+            return x(d.salesperson)
+        })
         .attr("y", function (d) {
             return y(d.sales);
         })
@@ -213,67 +244,48 @@ updateChart = () => {
         .attr("width", 0)
         .attr("x", width)
         .remove()
-        // .attr("y", function (d) {
-        //     return y(d.sales);
-        // })
-        // .attr("height", 0)
+    // .attr("y", function (d) {
+    //     return y(d.sales);
+    // })
+    // .attr("height", 0)
 
     // .remove();
     // .remove()
 
+    var bars = d3.select("svg").select("g").selectAll("text")
+        .data(data);
+
+    bars.enter()
+
+        .append("text")
+        .text(function (d) {
+            return d.sales;
+        })
+        .attr("text-anchor", "middle")
+        .attr("x", function (d, i) {
+            return x(d.salesperson) + x.bandwidth() / 2;
+        })
+        .attr("y", function (d) {
+            return y(d.sales) - 10;
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("fill", "black")
+        .merge(bars)
+        .transition().duration(2000)
+        .text(function (d) {
+            return d.sales;
+        })
+        .attr("text-anchor", "middle")
+        .attr("x", function (d, i) {
+            return x(d.salesperson) + x.bandwidth() / 2;
+        })
+        .attr("y", function (d) {
+            return y(d.sales) - 10;
+        })
+
 
 }
-
-swapAxis = () => {
-
-    margin = {
-            top: 40,
-            right: 30,
-            bottom: 20,
-            left: 20
-        },
-        width = 500 - margin.left - margin.right,
-        height = 960 - margin.top - margin.bottom;
-
-
-    // append the svg object to the body of the page
-    // append a 'group' element to 'svg'
-    // moves the 'group' element to the top left margin
-    d3.select(".chart").select("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
-
-    var x = d3.scaleBand()
-        .range([height, 0])
-        .padding(0.1);
-    var y = d3.scaleLinear()
-        .range([0, width]);
-
-
-    data.forEach(function (d) {
-        d.sales = +d.sales;
-    });
-
-    y.domain(data.map(function (d) {
-        return d.salesperson;
-    }));
-    x.domain([0, d3.max(data, function (d) {
-        return d.sales;
-    })]);
-
-    d3.select(".xAxis")
-        // .attr("transform", "translate(0," + height + ")")
-        .transition().duration(2000)
-        .call(d3.axisLeft(y));
-
-    d3.select(".yAxis")
-        .attr("transform", "translate(0," + height + ")")
-        .transition().duration(2000)
-        .call(d3.axisBottom(x));
-}
-
 
 
 randomIntFromInterval = (min, max) => {
@@ -324,7 +336,7 @@ randomiseData = data => {
 }
 
 reorderData = data => {
-    data.sort((a,b) => {
+    data.sort((a, b) => {
         return b.sales - a.sales;
     })
     return data;
@@ -332,4 +344,68 @@ reorderData = data => {
 
 randomName = () => {
     return Math.random().toString(36).substr(2, 5);
+}
+
+slideBars = () => {
+
+    //data = randomiseData(data);
+    data = reorderData(data);
+
+    data.forEach(function (d) {
+        d.sales = +d.sales;
+    });
+
+    x.domain(data.map((d) => {
+        return d.salesperson
+    }))
+
+    y.domain([0, d3.max(data, function (d) {
+        return d.sales;
+    })]);
+
+    svg.selectAll("rect")
+        .transition()
+        .duration(2000)
+        .attr("x", (d) => {
+            return x(d.salesperson);
+        })
+        .attr("y", (d) => {
+            return y(d.sales);
+        })
+        .attr("height", function (d) {
+            return height - y(d.sales);
+        })
+        .attr("fill", (d) => {
+            return getColor(1 - d.sales / 100);
+        });
+
+    svg.selectAll(".chartLabel")
+        .transition().duration(2000)
+        .text(function (d) {
+            return d.sales;
+        })
+        .attr("text-anchor", "middle")
+        .attr("x", function (d, i) {
+            return x(d.salesperson) + x.bandwidth() / 2;
+        })
+        .attr("y", function (d) {
+            return y(d.sales) - 10;
+        })
+
+
+    d3.select(".xAxis")
+        .attr("transform", "translate(0," + height + ")")
+        .transition().duration(2000)
+        .call(d3.axisBottom(x));
+
+        console.table(data);
+
+}
+
+loadData = () => {
+    d3.json("pl.json")
+    .then(data => {
+        console.log(data);
+    });
+
 }
